00b444ac490a889bf83c8a2760d2f8a327c16897
Peerapat Sattapornnara <93782805+Phepattt2@users.noreply.github.com>
2023-07-05 11:35:34 +0700

    add excel api

--- src/package_service/package_service.service.ts
+++ src/package_service/package_service.service.ts
@@ -13,19 +13,62 @@ export class PackageServiceService {
     ) { }
 
     async insertPackageService(packageService: packageServiceEntity): Promise<packageServiceEntity> {
-       
-        const duplicate = await this.packageServiceRepository.findOneBy({ package_id : packageService.package_id})
-        if(!duplicate){
+
+        const duplicate = await this.packageServiceRepository.findOneBy({ package_id: packageService.package_id })
+
+        if (!duplicate) {
             packageService.created_at = new Date();
 
             packageService.updated_at = new Date();
-    
+
             return await this.packageServiceRepository.save(packageService);
-        }else {
-            return null ; 
+        } else {
+            return null;
         }
 
     }
+
+    async insertPackageServiceExcel(packageService: packageServiceEntity[]): Promise<string> {
+
+        try {
+        
+            const queryResult = await this.packageServiceRepository.findBy({ service_id: packageService[0].service_id })
+
+            for (let i = 0; i < packageService.length; i++) {                
+
+                for ( const row of queryResult) {
+                    if( row.package_id == packageService[i].package_id){
+                        row.package_name_en = packageService[i].package_name_en
+                        row.package_name_th = packageService[i].package_name_th
+                        row.package_price =packageService[i].package_price
+                        row.package_type =packageService[i].package_type
+                        row.package_validity =packageService[i].package_validity
+                        row.package_validity_unit = packageService[i].package_validity_unit
+                        packageService[i] = row ;
+                    }
+                }
+
+                packageService[i].created_at = new Date();
+
+                packageService[i].updated_at = new Date();
+
+            await this.packageServiceRepository.save(packageService[i]).then((res)=>{console.log(res)});
+                
+            }
+            
+
+            return "insert success";
+        
+        } catch (e){
+            console.log(e);
+            return "error"
+        }
+
+    }
+
+
+
+
 
     async findAll(): Promise<packageServiceEntity[]> {
         return await this.packageServiceRepository.find();
@@ -34,10 +77,10 @@ export class PackageServiceService {
     async deleteById(package_service: packageServiceEntity): Promise<string> {
         try {
             const delRes = await this.packageServiceRepository.createQueryBuilder()
-            .delete()
-            .where('service_id = :value1 AND package_id = :value2' , {value1 : package_service.service_id ,value2 : package_service.package_id})
-            .execute();
-            if(delRes.affected == 0 ){
+                .delete()
+                .where('service_id = :value1 AND package_id = :value2', { value1: package_service.service_id, value2: package_service.package_id })
+                .execute();
+            if (delRes.affected == 0) {
                 return "packageService delete failled not found"
             }
             return "packageService successfully deleted"
@@ -81,24 +124,24 @@ export class PackageServiceService {
         }
     }
 
-    async searchBy(entity : packageServiceEntity): Promise<packageServiceEntity[]> {
-        const found = await this.packageServiceRepository.findBy(entity) ; 
-            if (found) {
-                return found
-            } else {
-                return null ; 
-            }
+    async searchBy(entity: packageServiceEntity): Promise<packageServiceEntity[]> {
+        const found = await this.packageServiceRepository.findBy(entity);
+        if (found) {
+            return found
+        } else {
+            return null;
+        }
     }
-  
+
 
 
 }
 async function initerUpdate(notIncludeList: string[], userInput: packageServiceEntity, baseInput: packageServiceEntity): Promise<packageServiceEntity> {
-   const exportedObject = baseInput;
-   for (var key in userInput) {
-       if (notIncludeList.includes(key) == false) {
-           exportedObject[key] = userInput[key];
-       }
-   }
-   return exportedObject
+    const exportedObject = baseInput;
+    for (var key in userInput) {
+        if (notIncludeList.includes(key) == false) {
+            exportedObject[key] = userInput[key];
+        }
+    }
+    return exportedObject
 }
